08ead542374ea88cd7665b9be41ed254fc4d4fa5
Giorubio27 <Giorubio27@gmail.com>
2026-03-25 18:48:05 -0600

    added showProjectsByorganization to controller and created the projects.ejs page

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,5 +1,7 @@
 // Import any needed model functions
 import { getAllProjects } from '../models/projects.js';
+import { getProjectsByOrganizationId } from '../models/projects.js';
+
 
 // Define any controller functions
 const showProjectsPage = async (req, res) => {
@@ -9,5 +11,16 @@ const showProjectsPage = async (req, res) => {
     res.render('projects', { title, projects });
 };
 
+const showProjectsByOrganization = async (req, res) => {
+    const organizationId = req.params.organizationId;
+    const projects = await getProjectsByOrganizationId(organizationId);
+    const title = 'Service Projects';
+
+    res.render('projects', { title, projects });
+};
+
+
+
+
 // Export any controller functions
-export { showProjectsPage };
+export { showProjectsPage, showProjectsByOrganization };
